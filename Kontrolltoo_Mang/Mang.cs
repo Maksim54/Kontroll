00bfa7c2624b7b01e9ee941440a3fa01638a1df6
opilane <opilane@HP-CZC6297B8C>
2022-09-14 11:36:22 +0300

    Add project files.

---
+++ Kontrolltoo_Mang/Mang.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontrolltoo_Mang
+{
+    internal class Mang 
+    {
+        private Tegelane[] characters;
+        public Mang(Tegelane[] characters)
+        {
+            this.characters = characters;
+        }
+        //public List <Tegelane> SuurimaEsemeteArvuga()
+        //{
+        //    Tegelane player = characters[];
+        //    List<Tegelane> out_list = new List<Tegelane>();
+        //    foreach (Tegelane item in characters)
+        //    {
+        //        int check = player.CompareTo(item);
+        //        if (check == 1)
+        //        {
+        //            player = item;
+        //            out_list.Clear();
+        //        }
+        //        if (check != 1)
+        //        {
+        //            out_list.Add(item);
+        //        }
+
+        //    }
+        //    out_list.Add(player);
+        //    return out_list;
+        //}
+        //public Tegelane suurimaPunktideArvuga() 
+        //{
+        //    foreach (Tegelane item in characters) 
+        //    {
+        //        Tegelane b = ;
+        //        if ()
+        //        {
+
+        //        }
+        //    }
+        //}
+
+    }
+}
---
+++ Kontrolltoo_Mang/Tegelane.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontrolltoo_Mang
+{
+    class Tegelane : Uksus, IComparable<Tegelane>
+    {
+        private string nimi;
+        private List<Ese> ese_list = new List<Ese>();
+        protected double m_value = 0.0;
+        public Tegelane(string nimi)
+        {
+            this.nimi = nimi;
+        }
+        public int addItem(int item) 
+        { return item; }
+        public int punktideArv()
+        {
+            int arv_sum = 0;
+            foreach (Ese item in ese_list)
+            {
+                arv_sum += item.punktideArv(); 
+            }
+            return arv_sum;
+        }
+        //public int punktideArv_(List <Ese> list)
+        //{
+            
+        //    foreach (Ese item in list)
+        //    {
+        //        arv_sum_ += item.punktideArv();
+        //    }
+        //    return arv_sum_;
+        //}
+
+        public string info()
+        {
+            Console.WriteLine($"Nimi: {nimi}\nArvu punktide {punktideArv()}");
+            return $"Nimi: {nimi}\nArvu punktide {punktideArv()}";
+        }
+        public void valjastaEsemed() 
+        {
+            Console.WriteLine("Esemed:");
+            foreach (Ese item in ese_list)
+            {
+                Console.WriteLine(item.info());
+            }
+            Console.WriteLine();
+        }
+
+        public int CompareTo(Tegelane? other)
+        {
+            if (other == null) return 1;
+            return m_value.CompareTo(other.m_value);
+        }
+        public void issueItems(List<Ese> list, int item)
+        {
+            Ese help_check = list[item];
+            ese_list.Add(list[item]);
+            
+
+        }
+    }
+}
